2a8361d91e1013b3ad5316a456ccc548322fa6ea
Jose Orozco <jose816orozco@gmail.com>
2021-05-12 13:34:19 -0500

    create function to write to readme file

--- index.js
+++ index.js
@@ -1,45 +1,101 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const path = require('path')
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
 inquirer.prompt([
   {
     type: "input",
+    message: "Project Title?",
+    name: "title",
+  },
+  
+  {
+    type: "input",
+    message: "Project Description?",
+    name: "description",
+  },
+
+    {
+    type: "input",
     message: "What was your motivation?",
-    name: "",
+    name: "motivation",
   },
 
   {
     type: "input",
     message: "Why did you build this project?",
-    name: "",
+    name: "build",
   },
 
   {
     type: "input",
     message: "What problem does it solve",
-    name: "",
+    name: "solve",
   },
 
   {
     type: "input",
     message: "What did you learn?",
-    name: "",
+    name: "learn",
   },
 
   {
     type: "input",
     message: "What makes your project stand out?",
-    name: "",
+    name: "standout",
   },
-]);
+
+  {
+    type: "list",
+    message: "choose type of project license?",
+    name: "license",
+    choices: [
+        "MIT",
+        "Other",
+        "GPLv2",
+        "Apache",
+        "GPLv3",
+        "BSD 3-clause",
+        "Unlicense",
+        "BSD 2-clause",
+        "LGPLv3",
+        "AGPLv3",
+      ],
+  },
+
+])
+
+.then((data)=> {
+    console.log(data)
+    writeToFile('README.md', generateMarkdown(data))
+    // const{
+    //     title,
+    //     description,
+    //     motivation,
+    //     build,
+    //     solve,
+    //     learn,
+    //     standout,
+    //     license,
+    // } = data;
+    // fs.writeToFile()
+})
+
+
+
+
+
+
+
 // const questions = [];
 
 // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-inquirer.prompt(questions).then((res) => {
-  fs.writeToFile("");
-});
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
+
 
 // TODO: Create a function to initialize app
 function init() {}
